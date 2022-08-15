b64795be0324eb854ad1191b380aff6fbe81690f
JLGM <juss.jlgm@gmail.com>
2022-08-15 20:39:32 -0300

    cambiando valores de respuestas en adivinar

--- JS/dom1.js
+++ JS/dom1.js
@@ -6,20 +6,18 @@ formulario.addEventListener('submit', adivinar);
 
 function comenzarJuego(){
    numeroAdivinar = Math.floor(Math.random()*(6-1 +1) + 1)
-    console.log(numeroAdivinar)
 }
 
 function adivinar(e){
     e.preventDefault();
-    console.log('desde la funcion submit');
     let numeroIngresado = document.querySelector('#inputAdivinar').value;
     console.log(numeroIngresado)
     if(numeroIngresado == numeroAdivinar){
         alert('Adivinaste!')
     }else if (numeroAdivinar < numeroIngresado){
-        alert("¡Fallaste! Tu número " +numeroIngresado+ " es mayor al número mágico " +numeroAdivinar+".")
+        alert("¡Fallaste! Tu número " +numeroIngresado+ " es mayor al número mágico ")
     }
     else{
-        alert("¡Fallaste! Tu número " +numeroIngresado+ " es menor al número mágico " +numeroAdivinar+".")
+        alert("¡Fallaste! Tu número " +numeroIngresado+ " es menor al número mágico ")
     }
 }
